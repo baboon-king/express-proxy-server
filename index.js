a367e56cf4c0138dcdbd97d2cefd91063c037c11
changfan.xu <changfan.xu.up@qq.com>
2020-09-10 19:12:39 +0800

    update

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const bodyParser = require('body-parser')
 const superagent = require('superagent')
 const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// create application/json parser
+app.use(bodyParser.json({ limit: '50mb' }));
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+
 
 // target api
 const api = 'http://www.xxxxxxxxx.cn/'
@@ -29,26 +32,40 @@ app.get('/', function (req, res) {
     res.send('用于转发接口使用')
   }
 })
-app.get('*', callback);
-app.post('*', callback);
-app.listen(8082);
-
-function callback(req, res) {
+app.get('*', (req, res) => {
   const nres = res
-  const data = req.query
+  const query = req.query
   const ServerCookie = req.headers.cookie
   const sreq = superagent.get(api + req.url)
   //如果使用 Accept存在的的话则设置为Accept的值
   sreq.set({
-    'Accept': data.Accept || 'application/json',
+    'Accept': req.headers.accept || 'application/json',
     'Content-Type': 'application/json;charset=UTF-8'
   })
   ServerCookie && sreq.set('Cookie', ServerCookie)
-  data.datas && sreq.query(data.datas)
+  query && sreq.query(query)
   sreq.end((err, res) => {
     handleResp(err, nres, res);
   })
-}
+});
+app.post('*', (req, res) => {
+  const nres = res
+  const body = req.body
+  const ServerCookie = req.headers.cookie
+  const sreq = superagent.get(api + req.url)
+  //如果使用 Accept存在的的话则设置为Accept的值
+  sreq.set({
+    'Accept': req.headers.accept || 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8'
+  })
+  ServerCookie && sreq.set('Cookie', ServerCookie)
+  body && sreq.query(body)
+  sreq.end((err, res) => {
+    handleResp(err, nres, res);
+  })
+});
+
+app.listen(8082);
 
 function handleResp(err, nres, res) {
   if (err) {
